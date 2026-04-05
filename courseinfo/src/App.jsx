8faae4b7c6f23300161804950b4c8516c0da77cc
springbamboo <wuchengzhong1125@gmail.com>
2026-04-05 17:49:17 +0900

    exercise 1.5

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -4,7 +4,7 @@ const Header = ({course}) => {
 }
 
 const Part = ({part}) => {
-  return <p>{part.name} {part.exercise}</p>
+  return <p>{part.name} {part.exercises}</p>
 }
 
 const Content = ({parts}) => {
@@ -22,27 +22,29 @@ const Total = ({parts}) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
  
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
