3e9cdb15278723fa858be90877c3037ecbf41707
springbamboo <wuchengzhong1125@gmail.com>
2026-04-05 18:20:49 +0900

    exercise 1.6

---
+++ unicafe/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {setGood(good + 1)}
+  const handleNeutralClick = () => {setNeutral(neutral + 1)}
+  const handlebadClick = () => {setBad(bad + 1)}
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handlebadClick}>bad</button>
+      <h1>statistics</h1>
+      <div>
+        good {good}
+      </div>
+      <div>
+        neutral {neutral}
+      </div>
+      <div>
+        bad {bad}
+      </div>
+    </div>
+  )
+}
+
+export default App
