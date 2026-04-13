4d7c7a32053ab7d3117fd84cc8f78122b6023232
springbamboo <wuchengzhong1125@gmail.com>
2026-04-14 00:43:21 +0900

    exercise 2.1

---
+++ courseinfo2/src/App.jsx
@@ -0,0 +1,58 @@
+const Header = ({header}) => {
+  return (
+    <h1>{header}</h1>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <>
+      <Part part={parts[0]}/>
+      <Part part={parts[1]}/>
+      <Part part={parts[2]}/>
+    </>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <>
+      <Header header={course.name}/>
+      <Content parts={course.parts}/>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
