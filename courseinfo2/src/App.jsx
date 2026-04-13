ccb9d11e91b41da2adf9c10f2223362359a3970b
springbamboo <wuchengzhong1125@gmail.com>
2026-04-14 01:34:20 +0900

    exercise 2.4

--- courseinfo2/src/App.jsx
+++ courseinfo2/src/App.jsx
@@ -6,11 +6,7 @@ const Header = ({header}) => {
 
 const Content = ({parts}) => {
   return (
-    <>
-      <Part part={parts[0]}/>
-      <Part part={parts[1]}/>
-      <Part part={parts[2]}/>
-    </>
+    parts.map(item => <Part part={item} key={item.id}/>)
   )
 }
 
@@ -36,29 +32,54 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    courses.map((item) => <Course course={item} key={item.id}/>)
+  )
 }
 
 export default App
