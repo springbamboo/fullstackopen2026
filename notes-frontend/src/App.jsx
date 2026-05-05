28e70ea0a89782ecd53fbbe2dedea8294d58aec3
springbamboo <wuchengzhong1125@gmail.com>
2026-05-06 01:12:05 +0900

    complete part3 learning

--- notes-frontend/src/App.jsx
+++ notes-frontend/src/App.jsx
@@ -22,8 +22,7 @@ const App = () => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5,
-      id: String(notes.length + 1)
+      important: Math.random() < 0.5
     }
     noteService
       .create(noteObject)
