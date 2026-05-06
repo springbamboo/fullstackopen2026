00bf8841bb19eacd9fa3a006399ed2e013c5a9e9
springbamboo <wuchengzhong1125@gmail.com>
2026-05-06 19:21:47 +0900

    exercise 3.7&3.8

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
+
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body'),
+);
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
