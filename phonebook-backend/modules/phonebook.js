f1e118db496f43c5dda03289bc70e65fb5486ad2
springbamboo <wuchengzhong1125@gmail.com>
2026-05-10 23:33:31 +0900

    exercise 3.22

--- phonebook-backend/modules/phonebook.js
+++ phonebook-backend/modules/phonebook.js
@@ -1,39 +1,39 @@
-import mongoose from 'mongoose';
-import 'dotenv/config';
+import mongoose from 'mongoose'
+import 'dotenv/config'
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
 mongoose
-  .connect(url, {family: 4})
+  .connect(url, { family: 4 })
   .then(() => {
-    console.log('connected to MongoDB');
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.error('error connecting to MongoDB:', error.message);
-  });
+    console.error('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-  name: {type: String, minLength: 8},
+  name: { type: String, minLength: 8 },
   number: {
     type: String,
     validate: {
       validator: function (v) {
-        return /^\d{2,3}-\d+$/.test(v);
+        return /^\d{2,3}-\d+$/.test(v)
       },
     },
   },
-});
+})
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema)
 
-export default Person;
+export default Person
