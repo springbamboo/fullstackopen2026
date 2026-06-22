a6376d80d4f0c1d87be3dc4d8bd2cd92039edf3c
springbamboo <wuchengzhong1125@gmail.com>
2026-06-23 02:38:43 +0900

    exercise 4.5

--- blog/blog-backend/utils/list_helper.js
+++ blog/blog-backend/utils/list_helper.js
@@ -5,4 +5,12 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
   return blogs.reduce((pre, cur) => pre + cur.likes, 0);
 };
-export {dummy, totalLikes};
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce(
+    (favorite, current) =>
+      current.likes > favorite.likes ? current : favorite,
+    blogs[0],
+  );
+};
+export {dummy, totalLikes, favoriteBlog};
