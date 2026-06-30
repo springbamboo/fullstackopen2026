607695432b9175854d31e653278c5cf13409ae28
springbamboo <wuchengzhong1125@gmail.com>
2026-06-30 13:39:02 +0900

    add try-catch

--- blog/blog-backend/controllers/blog.js
+++ blog/blog-backend/controllers/blog.js
@@ -3,28 +3,39 @@ import Blog from '../modules/blog.js';
 
 const blogRouter = express.Router();
 
-blogRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogRouter.get('/', async (request, response, next) => {
+  try {
+    const allBlogs = await Blog.find({});
+    return response.status(200).json(allBlogs);
+  } catch (error) {
+    next(error);
+  }
 });
 
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response, next) => {
   const blog = new Blog({...request.body, likes: request.body.likes ?? 0});
 
   if (!(request.body.title && request.body.url)) {
     return response.status(400).json({error: 'title and url are required'});
   }
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  try {
+    blog.save().then((result) => {
+      response.status(201).json(result);
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
-blogRouter.delete('/:id', async (request, response) => {
+blogRouter.delete('/:id', async (request, response, next) => {
   const blogID = request.params.id;
-  await Blog.findByIdAndDelete(blogID);
-  return response.status(204).end();
+  try {
+    await Blog.findByIdAndDelete(blogID);
+    return response.status(204).end();
+  } catch (error) {
+    next(error);
+  }
 });
 
 blogRouter.put('/:id', async (request, response, next) => {
