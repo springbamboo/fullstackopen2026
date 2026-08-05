bc18dfd1ffbc2840b7d26e2a17bdccd92c6aab31
springbamboo <wuchengzhong1125@gmail.com>
2026-08-06 01:56:57 +0900

    exercise 12.14

--- fs-containers/todo-app/todo-frontend/eslint.config.js
+++ fs-containers/todo-app/todo-frontend/eslint.config.js
@@ -15,7 +15,7 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {...globals.browser, ...globals.vitest},
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -26,4 +26,4 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
-])
+]);
