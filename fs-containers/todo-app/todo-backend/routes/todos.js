e5954005a2d2fb87e71289776024490e5df7d738
springbamboo <41501269+springbamboo@users.noreply.github.com>
2026-08-09 16:34:25 +0900

    Part12 (#8)
    * initial setup
    
    * exercise 12.2
    
    * exercise 12.3
    
    * exercise 12.4
    
    * exercise 12.5
    
    * exercise 12.5 add dockerignore
    
    * exercise 12.6
    
    * exercise 12.7
    
    * exercise 12.8
    
    * exercise 12.9
    
    * exercise 12.11
    
    * exercise 12.10
    
    * exercise 12.12
    
    * exercise 12.13
    
    * exercise 12.14
    
    * exercise 12.15
    
    * exercise 12.16
    
    * exercise 12.17
    
    * exercise 12.18&12.19
    
    * exercise 12.20
    
    * exercise 12.21

---
+++ fs-containers/todo-app/todo-backend/routes/todos.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const { Todo } = require('../mongo')
+const {set, get} = require('../redis')
+const router = express.Router();
+
+/* GET todos listing. */
+router.get('/', async (_, res) => {
+  const todos = await Todo.find({})
+  res.send(todos);
+});
+
+router.get('/statistics', async (req, res) => {
+  const addedTodos = await get('added_todos');
+  return res.status(200).json({added_todos: Number(addedTodos) || 0})
+});
+
+/* POST todo to listing. */
+router.post('/', async (req, res) => {
+  const todo = await Todo.create({
+    text: req.body.text,
+    done: false
+  })
+  const currentCount = await get('added_todos');
+  const newCount = (Number(currentCount) || 0) + 1;
+  await set('added_todos', newCount);
+  res.send(todo);
+});
+
+const singleRouter = express.Router();
+
+const findByIdMiddleware = async (req, res, next) => {
+  const { id } = req.params
+  req.todo = await Todo.findById(id)
+  if (!req.todo) return res.sendStatus(404)
+
+  next()
+}
+
+/* DELETE todo. */
+singleRouter.delete('/', async (req, res) => {
+  await req.todo.delete()  
+  res.sendStatus(200);
+});
+
+/* GET todo. */
+singleRouter.get('/', async (req, res) => {
+  const todo = await req.todo;
+  return res.status(200).json(todo)
+});
+
+/* PUT todo. */
+singleRouter.put('/', async (req, res) => {
+  if (req.body.text !== undefined) req.todo.text = req.body.text;
+  if (req.body.done !== undefined) req.todo.done = req.body.done;
+
+  const savedTodo = await req.todo.save();
+  return res.status(200).json(savedTodo);
+});
+
+router.use('/:id', findByIdMiddleware, singleRouter)
+
+
+module.exports = router;
